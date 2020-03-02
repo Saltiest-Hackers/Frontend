f89c40b03012f9d139629275a2f81d25eb3cbec7
unknown <jacob.e.england@gmail.com>
2020-03-02 16:47:40 -0600

    Added sort function

--- saltiest-hackers/src/components/Feed.js
+++ saltiest-hackers/src/components/Feed.js
@@ -1,15 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import dummy from '../assets/MOCK_DATA.json'
 import Comment from './Comment';
 
 const Feed = () => {
     const [data] = useState(dummy);
-    const [display, setDisplay] = useState(dummy);
+    const [display, setDisplay] = useState([]);
+    useEffect(() => {
+        setDisplay(data);
+    }, [data])
+    const sortBySaltiness = () => {
+        const newArr = [ ...data ];
+        newArr.sort((a, b) => {
+            if (a.saltiness > b.saltiness) {
+                return 1;
+            }
+            else if (a.saltiness < b.saltiness) {
+                return -1;
+            }
+            return 0;
+        });
+        setDisplay(newArr);
+    }
     return (
         <React.Fragment>
             <h1>Comment Feed</h1>
-            {data.map((comment) => {
+            <button onClick={() => sortBySaltiness()}>Sort</button>
+            {display.map((comment) => {
                 return (
                     <Comment comment={comment} />
                 )
