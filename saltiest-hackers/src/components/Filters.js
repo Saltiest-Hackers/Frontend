f2457f7cb12d5c2d09c4feb668423014f8715aa1
unknown <jacob.e.england@gmail.com>
2020-03-03 16:24:07 -0600

    Put sorting logic in select menu instead of buttons

--- saltiest-hackers/src/components/Filters.js
+++ saltiest-hackers/src/components/Filters.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import { Button, ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Typography, TextField } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Button, ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, MenuItem, Select, Typography, TextField } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const Filters = (props) => {
-    const sortBySaltiness = () => {
+    // Function to sort list either by ascending or descending value; alphabetically or numerically
+    const sortBySaltiness = (direction, prop) => {
+        let num = 0;
+        if (direction === 'ascending') {
+            num = 1;
+        } else if (direction === 'descending') {
+            num = -1;
+        }
         const newArr = [ ...props.display ];
         newArr.sort((a, b) => {
-            if (a.saltiness > b.saltiness) {
-                return 1;
+            if (a[prop] > b[prop]) {
+                return (num);
             }
-            else if (a.saltiness < b.saltiness) {
-                return -1;
+            else if (a[prop] < b[prop]) {
+                return -(num);
             }
             return 0;
         });
@@ -23,6 +30,25 @@ const Filters = (props) => {
         })
         props.setDisplay(filtered);
     }
+    // Function containing all switch logic for filters
+    const filterSwitch = (sort) => {
+        if(sort) {
+            switch (sort) {
+                case 'ascSalt': 
+                    sortBySaltiness('ascending', 'saltiness');
+                    break;
+                case 'descSalt':
+                    sortBySaltiness('descending', 'saltiness');
+                    break;
+                case 'alpha':
+                    sortBySaltiness('ascending', 'username');
+                    break;
+                case 'revAlpha':
+                    sortBySaltiness('descending', 'username');
+                    break;
+            }
+        }
+    }
     const resetDisplay = () => props.setDisplay(props.data);
     return (
         <ExpansionPanel>
@@ -30,8 +56,14 @@ const Filters = (props) => {
                 <Typography>Filters</Typography>
             </ExpansionPanelSummary>
             <ExpansionPanelDetails>
-                <TextField onChange={(e) => filterByName(e.target.value)} />
-                <Button onClick={() => sortBySaltiness()} variant='outlined'>Sort</Button>
+                <Select onChange={(e) => filterSwitch(e.target.value)}>
+                    <MenuItem value='' disabled>Sort By</MenuItem>
+                    <MenuItem value='ascSalt'>Saltiness (low - high)</MenuItem>
+                    <MenuItem value='descSalt'>Saltiness (high - low)</MenuItem>
+                    <MenuItem value='alpha'>Alphabetically</MenuItem>
+                    <MenuItem value='revAlpha'>Reverse Alphabetically</MenuItem>
+                </Select>
+                <TextField onChange={(e) => filterByName(e.target.value)} label='Search' fullWidth={true} />
                 <Button onClick={() => resetDisplay()} variant='outlined'>Reset</Button>
             </ExpansionPanelDetails>
         </ExpansionPanel>
