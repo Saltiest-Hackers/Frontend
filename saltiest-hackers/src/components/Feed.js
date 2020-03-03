df1ddf879f8958a0702651cd68f005b0f30b82c2
unknown <jacob.e.england@gmail.com>
2020-03-03 14:57:48 -0600

    Added rudimentary styling to Feed.js

--- saltiest-hackers/src/components/Feed.js
+++ saltiest-hackers/src/components/Feed.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Typography } from '@material-ui/core';
+import { Button, Typography, makeStyles } from '@material-ui/core';
 
 import dummy from '../assets/MOCK_DATA.json'
 import Comment from './Comment';
 
+const useStyles = makeStyles({
+    title: {
+        color: 'white',
+        textAlign: 'center',
+        paddingTop: '3%'
+    }
+})
 
 const Feed = () => {
     const [data] = useState(dummy);
     const [display, setDisplay] = useState([]);
+    const classes = useStyles();
     useEffect(() => {
         setDisplay(data);
     }, [data])
@@ -26,7 +34,7 @@ const Feed = () => {
     }
     return (
         <React.Fragment>
-            <Typography variant='h4'>Comment Feed</Typography>
+            <Typography variant='h4' className={classes.title}>Comment Feed</Typography>
             <Button variant='contained' onClick={() => sortBySaltiness()}>Sort</Button>
             {display.map((comment, index) => {
                 return (
