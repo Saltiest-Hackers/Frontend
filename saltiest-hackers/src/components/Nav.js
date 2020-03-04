9b5076e64996a05fb9ed90dc6f9d3fc5174e8b0a
unknown <jacob.e.england@gmail.com>
2020-03-04 16:02:08 -0600

    Styled navbar

--- saltiest-hackers/src/components/Nav.js
+++ saltiest-hackers/src/components/Nav.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, makeStyles, withTheme } from '@material-ui/core';
 import { NavLink } from 'react-router-dom';
 
+const useStyles = makeStyles({
+    logo: {
+        fontSize: '1.8rem',
+    },
+    toolbar: {
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+    nav: {
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+    navLink: {
+        color: 'white',
+        textDecoration: 'none',
+        padding: '10px',
+        fontSize: '1.2rem',
+    },
+})
+
 const Nav = (props) => {
+    const classes = useStyles();
     return (
         <AppBar position='sticky'>
-            <Toolbar>
-                <Typography>🧂SH</Typography>
-                <nav>
-                    <NavLink to='/feed'>Home</NavLink>
-                    <NavLink to='/saved'>Saved</NavLink>
+            <Toolbar className={classes.toolbar}>
+                <Typography className={classes.logo}>🧂 Salty Hackers</Typography>
+                <nav className={classes.nav}>
+                    <NavLink className={classes.navLink} to='/feed'>Home</NavLink>
+                    <NavLink className={classes.navLink} to='/saved'>Saved</NavLink>
                 </nav>
             </Toolbar>
         </AppBar>
