fa7b33b067b672912d533f795be98bd7da955591
unknown <jacob.e.england@gmail.com>
2020-03-05 20:21:18 -0600

    Updates

---
+++ precache-manifest.8c20c6c5244e2c28d581ff8178c8d713.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86fbe4bd50e5c4bb8fec1b62cec2ccfb",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "11a7e93ace3b3f0616bb",
+    "url": "/frontend/static/css/main.c04885ce.chunk.css"
+  },
+  {
+    "revision": "ccc7cfdbd4318698912e",
+    "url": "/frontend/static/js/2.de90aa6e.chunk.js"
+  },
+  {
+    "revision": "43bf7255665fc148b09c4b201e7c13cd",
+    "url": "/frontend/static/js/2.de90aa6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11a7e93ace3b3f0616bb",
+    "url": "/frontend/static/js/main.6b81642c.chunk.js"
+  },
+  {
+    "revision": "e60b151174562e1dea8b",
+    "url": "/frontend/static/js/runtime-main.eef0a21f.js"
+  },
+  {
+    "revision": "5d746b278990697151bb59ee6b75018c",
+    "url": "/frontend/static/media/background.5d746b27.jpg"
+  }
+]);
